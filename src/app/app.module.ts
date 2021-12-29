eb972e58806f336ba172095edd361346102aa708
aryankumar123 <aryankumarben@gmail.com>
2021-12-29 10:26:30 +0530

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule } from "@angular/forms";
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDialogModule} from '@angular/material/dialog';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastrModule } from 'ngx-toastr';
+
+import { OrdersComponent } from './orders/orders.component';
+import { OrderComponent } from './orders/order/order.component';
+import { OrderItemsComponent } from './orders/order-items/order-items.component';
+import { OrderService } from './shared/order.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FaceComponent } from './face/face.component';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    OrdersComponent,
+    FaceComponent,
+    OrderComponent,
+    FooterComponent,
+    OrderItemsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    BrowserAnimationsModule,
+    MatDialogModule,
+    HttpClientModule,
+    ToastrModule.forRoot()
+  ],
+  entryComponents:[OrderItemsComponent],
+  providers: [OrderService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/face/face.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'face',
+  templateUrl: './face.component.html',
+  styleUrls: ['./face.component.css']
+})
+export class FaceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
